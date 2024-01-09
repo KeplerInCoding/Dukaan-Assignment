8ecb5b1db0ef99e9b24b35fa2a6a6269e3bc59b3
KeplerInCoding <shreyakv8@gmail.com>
2024-01-09 12:53:07 +0530

    sidebar setup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from './Logo';
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
+    const buttonNames = ['Home','Orders','Products','Delivery','Marketing','Analytics','Payments','Tools','Discounts','Audience',
+        'Appearance','Plugins','Available credits',];
+      
+      return (
+    <div className=" bg-blue text-white">
+        <Logo/>
+        <nav>
+            <ul>
+                
+                {buttonNames.map((name, index) => (
+                    <li key={index}>
+                    <NavLink to={name === 'Discounts' ? '/' : `/progress`}>{name}</NavLink>
+                    </li>
+                ))}
+
+
+            </ul>
+        </nav>
+    </div>
   )
 }
 
