76d608ede3824ffcb931c696cbd026d59a72c1d9
KeplerInCoding <shreyakv8@gmail.com>
2024-01-09 12:18:46 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Discounts from "./pages/Discounts";
+import {Route, Routes} from 'react-router-dom';
+import Progress from "./pages/Progress";
 
 function App() {
-  return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
-  );
+  return (<div className="w-screen h-screen">
+    <div  className="flex">
+    <Sidebar/>
+
+    <Routes>
+        <Route path="/" element={<Discounts/>}></Route>
+        <Route path="/progress" element={<Progress/>}></Route>
+        {/* <Route path="/sip" element={<Delievery/>}></Route>
+        <Route path="/sip" element={<Marketing/>}></Route>
+        <Route path="/sip" element={<Analytics/>}></Route>
+        <Route path="/payouts" element={<Payouts/>}></Route>
+        <Route path="/discounts" element={<Discounts/>}></Route>
+        <Route path="/audience" element={<Audience/>}></Route>
+        <Route path="/appeareance" element={<Appeareance/>}></Route>
+        <Route path="/plugins" element={<Plugins/>}/> */}
+      </Routes>
+    </div>
+  </div>);
 }
 
 export default App;
---
+++ src/pages/Progress.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Progress = () => {
+  return (
+    <div>Page under development. Meanwhile you can view Discount Page</div>
+  )
+}
+
+export default Progress
